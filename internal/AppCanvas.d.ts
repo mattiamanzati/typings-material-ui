2f9af4711ccfc598631f8e54e714470e61917de7
Mattia Manzati <mattia.manzati@gmail.com>
2016-08-06 10:22:58 +0200

    feat: internal AppCanvas

---
+++ internal/AppCanvas.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react'
+
+export interface AppCanvasProps extends React.Props<AppCanvas> {
+}
+export default class AppCanvas extends React.Component<AppCanvasProps, {}> {
+}
