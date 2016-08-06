294fc1e610bfd50a7d9e4bf16b1b0d366e7eae9f
Mattia Manzati <mattia.manzati@gmail.com>
2016-08-06 10:26:39 +0200

    feat: BeforeAfterWrapper

---
+++ internal/BeforeAfterWrapper.d.ts
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+export interface BeforeAfterWrapperProps extends React.Props<BeforeAfterWrapper> {
+    afterElementType?: string;
+    afterStyle?: React.CSSProperties;
+    beforeElementType?: string;
+    beforeStyle?: React.CSSProperties;
+    elementType?: string;
+    style?: React.CSSProperties;
+}
+export default class BeforeAfterWrapper extends React.Component<BeforeAfterWrapperProps, {}> {
+}
